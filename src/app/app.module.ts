3cddd6471b0d05d01b194c59223a201dab1eaf89
Jerome BERTRAND <jerome.bertrand@pouey.com>
2023-03-06 14:12:50 +0100

    story + feeds added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { MenuComponent } from './common/menu/menu.component';
+import { StoriesComponent } from './common/stories/stories.component';
+import { FeedsComponent } from './home/feeds/feeds.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    MenuComponent
+    MenuComponent,
+    StoriesComponent,
+    FeedsComponent
   ],
   imports: [
     BrowserModule,
