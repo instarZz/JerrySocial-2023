7d24637592b8765cc128103aa54144952fb6d982
Jerome BERTRAND <jerome.bertrand@pouey.com>
2023-03-06 11:17:34 +0100

    styles added with root reset and coponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MenuComponent } from './menu/menu.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { MenuComponent } from './common/menu/menu.component';
 
 @NgModule({
   declarations: [
